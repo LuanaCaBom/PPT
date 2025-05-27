23fa2de913030da7590beb1c781eb87572537ed3
Luana <luacabom@gmail.com>
2025-05-27 17:02:56 -0300

    'nFinalizado'

--- App.tsx
+++ App.tsx
@@ -1,83 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, Image, TouchableOpacity,View, ImageSourcePropType} from 'react-native';
+import { Button, StyleSheet, Text, TextInput, Image, Pressable, View, ImageSourcePropType} from 'react-native';
 import {useState} from 'react';
 
-const imagens: ImageSourcePropType[] = [
-  require('./assets/tesoura-invertida.png'),
-  require('./assets/papel-invertido.png'),
+const imagens1: ImageSourcePropType[] = [
   require('./assets/pedra-invertida.png'),
-  require('./assets/tesoura.png'),
-  require('./assets/papel.png'),
-  require('./assets/pedra.png')
+  require('./assets/papel-invertido.png'),
+  require('./assets/tesoura-invertida.png')
 ]
 
-const[Esse, setEsse] = useState(0); 
+const imagens2: ImageSourcePropType[] = [
+  require('./assets/pedra.png'),
+  require('./assets/papel.png'),
+  require('./assets/tesoura.png')
+]
 
 export default function App() {
   const[Num, setNum] = useState(0);
-  //const[Anterior, setAnterior] = useState(0);
-  const[C, setC] = useState('W');
+  const[escolha, setEscolha] = useState('');
+  const[imagem, setImagem] = useState(0); 
+  const[texto, setTexto] = useState('');
   
-  function trocaEstado(){
-    let sorteio = 0;
-    do{
-      sorteio = Math.floor(Math.random() * 3);
-      console.log(sorteio, '-', Num);
-    }while(sorteio === Num)
-    setNum(sorteio);  
-    //setAnterior(Num);
 
-    if(sorteio < 3){
-      setC('yellow');
+  function oponente(){
+    setNum(Math.floor(Math.random() * 3));
+    
+  }
+
+  function status(){
+    if(imagem == Num){
+      setTexto('Empate!');
     }
-    else if(sorteio == 3 || sorteio == 4){
-      setC('blue');
+    else if((Num == 0 && imagem == 1) || (Num == 1 && imagem == 2) || (Num == 2 && imagem == 0)){
+      setTexto('Parabéns, você ganhou!');
     }
     else{
-      setC('white');
+      setTexto('Que pena, você perdeu!');
     }
   }
 
-  function esse(n: number){
-    if(n == 3){
-      setEsse(n);
+  function jogar(){
+    if(Number(escolha) == 1){
+      setImagem(0);
     }
-    if(n == 4){
-      setEsse(n);
+    else if(Number(escolha) == 2){
+      setImagem(1);
     }
-    else{
-      setEsse(n);
+    else if(Number(escolha) == 3){
+      setImagem(2);
     }
-
+    oponente();
+    status();
+    
   }
-
+  console.log(texto);
+  console.log(Num);
   return (
-    <View style={{backgroundColor: C, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-              <Text style={styles.texto}>Sorte</Text>
-      <TouchableOpacity onPress={trocaEstado}>
-
-        <Image style={styles.imagem} source={imagens[Num]}></Image>  
-
-      </TouchableOpacity>
-      <Image source={Esse}></Image>
-      <Text style={styles.texto}>Número sorteado: {Num + 1}</Text>
-      <Button title="Tesoura" onPress={()=> setEsse(3)} ></Button>
-      <Button title="Papel" onPress={()=> setEsse(4)} ></Button>
-      <Button title="Pedra" onPress={()=> setEsse(5)} ></Button>
+    <View style={styles.fundo}>
+      <Text style={styles.titulo}>Boa Sorte!</Text>
+      <Image style={styles.imagem} source={imagens1[Num]}></Image>
+      <Text style={styles.textoMeio}>{texto}</Text>
+      <Image style={styles.imagem} source={imagens2[Number(imagem)]}></Image>
+      <Text style={styles.texto}>1-Pedra   2-Papel   3-Tesoura</Text>
+      <TextInput  style={styles.input} value={escolha} 
+                  onChangeText={setEscolha} keyboardType='decimal-pad'
+                  placeholder='Digite o número de sua escolha:'/>
+      <Pressable style={styles.botao} onPress={jogar} ><Text style={styles.texto}>Jogar</Text></Pressable>
       <StatusBar style="auto" /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  titulo: {
+    color: '#ffffff',
+    fontSize: 35,
+    fontWeight: 'bold',
+    margin: 10,
+  },
   texto: {
     color: '#000',
-    fontSize: 40,
+    fontSize: 20,
     fontWeight: 'bold',
-    margin: 49,
+    margin: 10,
+  },
+  textoMeio: {
+    color: '#000',
+    fontSize: 26,
+    fontWeight: 'bold',
+    margin: 10,
   },
   imagem: {
     width: 200,
     height: 200,
   },
+  line: {
+    flexDirection: 'row'
+  },
+  input: {
+    backgroundColor: 'rgb(255, 255, 255)',
+    padding: 20,
+    margin: 10,
+    width: 280,
+    borderRadius: 8,
+    borderWidth: 3,
+    borderColor: 'black',
+  },
+  fundo: {
+    backgroundColor: 'rgb(0, 188, 235)', 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  botao: {
+    alignItems: "center",
+    width: 120,
+    justifyContent: "center",
+    padding: 5,
+    borderRadius: 5,
+    elevation: 3,
+    backgroundColor: 'rgb(221, 217, 0)',
+    borderWidth: 3,
+    marginTop: 10,
+  }
 });
